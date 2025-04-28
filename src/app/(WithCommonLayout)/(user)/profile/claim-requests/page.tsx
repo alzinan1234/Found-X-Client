45c97a9ad7bcff11a9a66414a1f137dd4037e066
johir-dev <pappurohan1100@gmail.com>
2025-04-28 21:52:28 +0600

    aadded

--- src/app/(WithCommonLayout)/(user)/profile/claim-requests/page.tsx
+++ src/app/(WithCommonLayout)/(user)/profile/claim-requests/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const page = () => {
   return (
     <div>
-      <h4> thiis is cleam request page </h4>
+      <h1> this is clieam request page </h1>
     </div>
   );
 };
--- src/app/(WithCommonLayout)/found-items/page.tsx
+++ src/app/(WithCommonLayout)/found-items/page.tsx
@@ -1,11 +1,16 @@
-import React from "react";
+import Container from "@/src/components/ui/Container";
+import Post from "@/src/components/UI/Post";
+import axiosInstance from "@/src/lib/AxiosInstance";
+import { IPost } from "@/src/types";
 
-const Page = () => {
+export default async function FoundItems() {
+  const { data } = await axiosInstance.get(`/items`);
+
   return (
-    <div>
-      <h2>this is found items </h2>
-    </div>
+    <Container>
+      <div className="mx-auto my-3 max-w-[720px]">
+        {data?.data?.map((post: IPost) => <Post key={post?._id} post={post} />)}
+      </div>
+    </Container>
   );
-};
-
-export default Page;
+}
