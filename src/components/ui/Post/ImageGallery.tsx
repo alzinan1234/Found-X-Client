f8f766dc244dce00d142b39deda82a51d1885bf8
johir-dev <pappurohan1100@gmail.com>
2025-05-07 15:44:38 +0600

    addeded

--- src/components/ui/Post/ImageGallery.tsx
+++ src/components/ui/Post/ImageGallery.tsx
@@ -1,8 +1,8 @@
 "use client";
+import LightGallery from "lightgallery/react";
 import "lightgallery/css/lightgallery.css";
 import "lightgallery/css/lg-zoom.css";
 import "lightgallery/css/lg-thumbnail.css";
-import LightGallery from "lightgallery/react";
 
 import lgThumbnail from "lightgallery/plugins/thumbnail";
 import lgZoom from "lightgallery/plugins/zoom";
